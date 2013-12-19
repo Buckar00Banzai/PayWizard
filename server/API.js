0617056c33cd505acc00de6bf8a2da7b07787d1b
cl0udc0ntr0l <randylebeau@gmail.com>
2013-12-19 01:16:49 -0300

    "font rollback"

--- server/API.js
+++ server/API.js
@@ -84,15 +84,79 @@ module.exports.api = function(server, Base, Ticket) {
 		function(req, res) {
 
 			var ticket = req.body.ticket,
-				payment = req.body.payment;
+				payment = req.body.payment,
+				j, ct;
+
+			switch(ticket.job) {
+				case dinnerPrep:
+					j = 'Dinner Preparation';
+					ct = '2pm';
+					break;
+				case dinnerClean:
+					j = 'Dinner Cleanup';
+					ct = '4pm';
+					break;
+				case brunchPrep:
+					j = 'Brunch Preparation';
+					ct = 'Be prepaired to stay until 2pm on the 1st.';
+					break;
+				case brunchClean:
+					j = 'Brunch Cleanup';
+					ct = 'Be prepaired to stay until 2pm on the 1st.';
+					break;
+				case altar:
+					j = 'Altar';
+					ct = '2pm';
+					break;
+				case templeSetup:
+					j = 'Temple Setup';
+					ct = '2pm';
+					break;
+				case templeBreakdown:
+					j = 'Temple Breakdown';
+					ct = 'Be prepaired to stay until 2pm on the 1st.';
+					break;
+				case pointman:
+					j = 'Point Person';
+					ct = '2pm';
+					break;
+				case tea:
+					j = 'Tea Preparation';
+					ct = '2pm';
+					break;
+				case teaSetup:
+					j = 'Tea Setup';
+					ct = '4pm';
+					break;
+				case music:
+					j = 'Musician';
+					ct = '2pm';
+					break;
+				case musicSetup:
+					j = 'Music Setup';
+					ct = '2pm';
+					break;
+				case musicBreakdown:
+					j = 'Music Breakdown';
+					ct = 'Be prepaired to stay until 2pm on the 1st.';
+					break;
+				case fire:
+					j = 'Firekeeper';
+					ct = '4pm';
+					break;
+			}
 
 			console.log(req.body);
 
 			var text = "Hey, this is a confirmation that your donation was accepted and a spot is being held for you at our gathering on New Year's Eve. Grab an extra blankie and an altar piece and alpaca your bags!\n\n";
-				text = text + "PAYMENT DETAILS:\n\n";
+				text = text + "DONATION DETAILS:\n\n";
 				text = text + "Total: $" + payment.transactions[0].amount.total + '\n';
 				text = text + "Card: " + payment.payer.funding_instruments[0].credit_card.type + ' ' + payment.payer.funding_instruments[0].credit_card.number + '\n';
-				text = text + "State: " + payment.state;
+				text = text + "State: " + payment.state + "\n\n";
+				text = text + "CONTRIBUTION DETAILS: \n\n";
+				text = text + "Activate: " + j + " (Call Time: " + ct + ")\n";
+				text = text + "Generate: " + ticket.food.toString().replace('/,/g', ', ') + "\n\n";
+				text = text + "Bring offerings of flowers, fruit, chocolate, candles, feathers, stones, sage, incense, words, laughter, songs, dances, and magic. Grandfather fire will be there to relieve you of anything you wish to leave behind along with 2013.";
 
 			var mailOptions = {
 				from: "Al the Alpaca ✔ <alchemicalalpaca@gmail.com>", // sender address
